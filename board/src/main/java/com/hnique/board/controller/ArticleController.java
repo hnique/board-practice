9cb39722c26cda94f1521cbfe5e66cdb9b29ca2c
hnique <hnique@naver.com>
2023-12-10 18:03:03 +0900

    feat: 데이터 수정 후 출력하기

--- board/src/main/java/com/hnique/board/controller/ArticleController.java
+++ board/src/main/java/com/hnique/board/controller/ArticleController.java
@@ -39,7 +39,28 @@ public class ArticleController {
         // System.out.println(saved.toString());
         log.info(saved.toString());
 
-        return "";
+        // 3. 상세 페이지로 리다이렉트
+        return "redirect:/articles/" + saved.getId();
+    }
+
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form) {
+        log.info(form.toString());
+        // 1. DTO를 Entity로 변환
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+
+        // 2. Entity를 DB에 저장
+        // 2-1. DB에서 기존 데이터 가져오기
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+
+        // 2-2. 기존 데이터 값을 갱신하기
+        if (target != null) {
+            articleRepository.save(articleEntity);
+        }
+
+        // 3. 수정 결과 페이지로 리다이렉트
+        return "redirect:/articles/" + articleEntity.getId();
     }
 
     @GetMapping("/articles/{id}")
@@ -65,7 +86,19 @@ public class ArticleController {
         // 2. 모델에 데이터 등록하기
         model.addAttribute("articleList", articleEntityList);
 
-        // 3. 뷰 페이지 설정하기
+        // 3. 뷰 페이지 반환하기
         return "articles/index";
+    }
+
+    @GetMapping("/articles/{id}/edit")
+    public String Edit(@PathVariable Long id, Model model) {
+        // 수정할 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        // 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+
+        // 뷰 페이지 반환하기
+        return "articles/edit";
     }
 }
